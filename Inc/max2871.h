446ec712856a2350e17df5af30c3bfe034fc2171
Filip Klapil <klapi.filip@seznam.cz>
2020-01-11 11:36:16 +0100

    max2871.c .h code refactored

--- Inc/max2871.h
+++ Inc/max2871.h
@@ -1,14 +1,14 @@
-/*************************************************************************
- *
- * $Id: ad9851.h 179 2007-09-16 15:02:06Z alpov $
- * 
- *************************************************************************/         
-
 #ifndef _MAX2871_H
 #define _MAX2871_H
 
 #include <stdint.h>
 
+#define PLO_DATA_SET HAL_GPIO_WritePin(PLO_DATA_GPIO_Port, PLO_DATA_Pin, GPIO_PIN_SET)
+#define PLO_DATA_RESET HAL_GPIO_WritePin(PLO_DATA_GPIO_Port, PLO_DATA_Pin, GPIO_PIN_RESET)
+#define PLO_CLK_SET HAL_GPIO_WritePin(PLO_CLK_GPIO_Port, PLO_CLK_Pin, GPIO_PIN_SET)
+#define PLO_CLK_RESET HAL_GPIO_WritePin(PLO_CLK_GPIO_Port, PLO_CLK_Pin, GPIO_PIN_RESET)
+#define PLO_LE_SET HAL_GPIO_WritePin(PLO_LE_GPIO_Port, PLO_LE_Pin, GPIO_PIN_SET)
+#define PLO_LE_RESET HAL_GPIO_WritePin(PLO_LE_GPIO_Port, PLO_LE_Pin, GPIO_PIN_RESET)
 
 typedef enum{
     PLO_NEW_DATA,
--- Src/max2871.c
+++ Src/max2871.c
@@ -6,53 +6,46 @@
 volatile plo_lock_state_t plo_lock_state = PLO_LOCK_STATE_WAIT;
 volatile plo_new_data_t plo_new_data = PLO_DATA_SENDED;
 
-/*************************************************************************/
+/*****************************************************************************/
 void plo_spi_emul(uint32_t data)
 {
-    for (uint8_t j = 0; j < 32; j++) {
-        if (data & 0x01)
-            HAL_GPIO_WritePin(PLO_DATA_GPIO_Port, PLO_DATA_Pin, GPIO_PIN_SET);
-        else
-            HAL_GPIO_WritePin(PLO_DATA_GPIO_Port, PLO_DATA_Pin, GPIO_PIN_RESET);
-        
+    for (uint8_t j = 0; j < 32; j++) 
+    {
+        (data & 0x01) ? PLO_DATA_SET : PLO_DATA_RESET;
+
         // generate clock pulse and shift next
-        HAL_GPIO_WritePin(PLO_CLK_GPIO_Port, PLO_CLK_Pin, GPIO_PIN_SET);
+        PLO_CLK_SET;
         data >>= 1;
-        HAL_GPIO_WritePin(PLO_CLK_GPIO_Port, PLO_CLK_Pin, GPIO_PIN_RESET);
+        PLO_CLK_RESET;
     }
     // generate update pulse
-    HAL_GPIO_WritePin(PLO_LE_GPIO_Port, PLO_LE_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(PLO_LE_GPIO_Port, PLO_LE_Pin, GPIO_PIN_RESET);
-    // set down all wires
-    HAL_GPIO_WritePin(PLO_DATA_GPIO_Port, PLO_DATA_Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(PLO_CLK_GPIO_Port, PLO_CLK_Pin, GPIO_PIN_RESET);
+    PLO_LE_SET;
+    PLO_LE_RESET;
+    // get down data wire
+    PLO_DATA_RESET;
 }
 
-/*************************************************************************/
+/*****************************************************************************/
 void plo_write_register(uint32_t register_data)
 {
     register_data = lsb_to_msb_bit_reversal(register_data);
-
     plo_spi_emul(register_data);
 }
 
-/*************************************************************************/
+/*****************************************************************************/
 void plo_write_all(uint32_t *max2871, plo_new_data_t plo_write_type)
 {
-    for (int8_t i = 5; i >= 0; i--) {
+    for (int8_t i = 5; i >= 0; i--)
+    {
         if (plo_write_type == PLO_OUT_ENABLE)
-        {
             i=4;
-        }
 
         uint32_t c = max2871[i];
-        if ((plo_write_type == PLO_INIT) && (i==4))
-        {
-            c &= ~((1UL << 5) | (1UL << 9 ));
-        }
-        c = lsb_to_msb_bit_reversal(c);
 
-        plo_spi_emul(c);
+        if ((plo_write_type == PLO_INIT) && (i==4))
+            c &= ~((1UL << 5) | (1UL << 9 ));
+
+        plo_spi_emul(lsb_to_msb_bit_reversal(c));
 
         if ((plo_write_type == PLO_INIT) && (i==5))
             HAL_Delay(20);
@@ -65,18 +58,8 @@ void plo_write_all(uint32_t *max2871, plo_new_data_t plo_write_type)
 void plo_check_lock_status(void)
 {
     if ( ( (test_data[2] & ((1<<28) | (1<<27) | (1<<26))) >> 26) == 0b110)
-    {
-        if (HAL_GPIO_ReadPin(PLO_MUXOUT_GPIO_Port, PLO_MUXOUT_Pin) == 1)
-        {   
-            plo_lock_state = PLO_LOCKED;
-        }
-        else
-        {
-            plo_lock_state = PLO_UNLOCKED;
-        }
-    }
+        (HAL_GPIO_ReadPin(PLO_MUXOUT_GPIO_Port, PLO_MUXOUT_Pin) == 1) ?
+            (plo_lock_state = PLO_LOCKED) : (plo_lock_state = PLO_UNLOCKED);
     else
-    {
         plo_lock_state = PLO_LOCK_STATE_UNKNOWN;
-    }
 }
